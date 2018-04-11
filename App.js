bdadf11f4fd902f6220cc55c401e8b9edb7f9dce
hengyang_King <hengyang.king@mymusichome.com>
2018-04-11 14:30:58 +0800

    flex

--- App.js
+++ App.js
@@ -26,76 +26,28 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      {/*flexDirection */}
-      <View style={viewStyles.container}>
-        <Text style = {{}}>*********flexDirection:"row" 属性********</Text>
-        <View style={viewStyles.box1}>
-          <Text>1</Text>
-          <Text>2</Text>
-          <Text>3</Text>
+      // 在组件样式中使用flex可以使其在可利用的空间中动态地扩张或收缩。
+      // 一般而言我们会使用flex:1来指定某个组件扩张以撑满所有剩余的空间。
+      // 如果有多个并列的子组件使用了flex:1，则这些子组件会平分父容器中剩余的空间。
+      // 如果这些并列的子组件的flex值不一样，则谁的值更大，
+      // 谁占据剩余空间的比例就更大（即占据剩余空间的比等于并列组件间flex值的比）
+      <View style = {{height:300,backgroundColor:"yellow",flexDirection:"row"}}>
+        <View style = {{flex:3,backgroundColor:"red",}}>
+          
         </View>
-
-        {/*justifyContent */}
-        <Text style = {{marginTop:20}}>*********justifyContent & alignItems 属性********</Text>
-        <View style = {viewStyles.box2}>
-          <Text style = {{height:20,backgroundColor:"green"}}>box2-0</Text>
-          <Text style = {{height:30,backgroundColor:"#010101"}}>box2-1</Text>
-          <Text style = {{height:40,backgroundColor:"#eeeeee"}}>box2-2</Text>
-          <Text style = {{height:50,backgroundColor:"#6e6e6e"}}>box2-3</Text>
+        <View style = {{flex:0.1,backgroundColor:"orange",}}>
+          
         </View>
-
-        {/*flexWrap */}
-        <View style = {{marginTop:20,backgroundColor:"red"}}>
-        <Text style = {{marginTop:20}}>*********flexWrap 属性********</Text>
-          <View style = {viewStyles.flex_wrap}>
-            <Text style = {{width:120,backgroundColor:"green"}}>box2-0</Text>
-            <Text style = {{width:230,backgroundColor:"yellow"}}>box2-1</Text>
-            <Text style = {{width:240,backgroundColor:"#eeeeee"}}>box2-2</Text>
-            <Text style = {{width:250,backgroundColor:"#6e6e6e"}}>box2-3</Text>
-          </View>
+        <View style = {{flex:0.1,backgroundColor:"#eeeeee",}}>
+          
         </View>
-          <Text>在默认情况下 项目都排在一条线上,flex-wrap属性定义 ，如果一条轴线无法排列，如何进行换行</Text>
-          <Text>分为 warp / no warp / warp-reverse</Text>
-        </View>
-
-        <View style = {viewStyles.description}>
-          <Text>flexDirection 定义元素的主轴方向</Text>  
-          <Text>justifyContent 定义伸缩项目在主轴线的对其方式</Text> 
-          <Text>alignItems 定义项目在侧轴上的对其方式</Text>   
-        </View>
-        
+      </View>
     );
   }
 }
 
 var viewStyles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor: '#F5FCFF',
-    justifyContent : "center",
-  },
-  box1 : {
-    marginTop:20,
-    backgroundColor:"green",
-    flexDirection:"row",
-    justifyContent : "space-between",
-  },
-  box2 : {
-    marginTop :30,
-    backgroundColor:"yellow",
-    flexDirection:"row",
-    alignItems:"center"
-
-  },
-  description : {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  flex_wrap : {
-    flexDirection:"row",
-    flexWrap:'wrap',
-
-  }, 
+  
 });
 
 
