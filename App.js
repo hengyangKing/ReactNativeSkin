f553e44f744ab1e2a89635a9834984ed236e99c3
hengyang_King <hengyang.king@mymusichome.com>
2018-04-11 11:06:13 +0800

    how 2 use flewbox

--- App.js
+++ App.js
@@ -22,68 +22,80 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+/**********************第一个示例程序****************************/
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-            Welcome to React Native！
-            啦啦啦
-
-        </Text>
-
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-
-        <Text style = {styles.welcome}>
-              你好世界123456
-        </Text>
-        <Text>
-          
-        </Text>
-        <View style = {styles2.viewStyle}>
-          <Text style = {styles2.textStyle}>
-            123
-          </Text>
+      {/*flexDirection */}
+      <View style={viewStyles.container}>
+        <Text style = {{}}>*********flexDirection:"row" 属性********</Text>
+        <View style={viewStyles.box1}>
+          <Text>1</Text>
+          <Text>2</Text>
+          <Text>3</Text>
         </View>
-      </View>
+
+        {/*justifyContent */}
+        <Text style = {{marginTop:20}}>*********justifyContent & alignItems 属性********</Text>
+        <View style = {viewStyles.box2}>
+          <Text style = {{height:20,backgroundColor:"green"}}>box2-0</Text>
+          <Text style = {{height:30,backgroundColor:"#010101"}}>box2-1</Text>
+          <Text style = {{height:40,backgroundColor:"#eeeeee"}}>box2-2</Text>
+          <Text style = {{height:50,backgroundColor:"#6e6e6e"}}>box2-3</Text>
+        </View>
+
+        {/*flexWrap */}
+        <View style = {{marginTop:20,backgroundColor:"red"}}>
+        <Text style = {{marginTop:20}}>*********flexWrap 属性********</Text>
+          <View style = {viewStyles.flex_wrap}>
+            <Text style = {{width:120,backgroundColor:"green"}}>box2-0</Text>
+            <Text style = {{width:230,backgroundColor:"yellow"}}>box2-1</Text>
+            <Text style = {{width:240,backgroundColor:"#eeeeee"}}>box2-2</Text>
+            <Text style = {{width:250,backgroundColor:"#6e6e6e"}}>box2-3</Text>
+          </View>
+        </View>
+          <Text>在默认情况下 项目都排在一条线上,flex-wrap属性定义 ，如果一条轴线无法排列，如何进行换行</Text>
+          <Text>分为 warp / no warp / warp-reverse</Text>
+        </View>
+
+        <View style = {viewStyles.description}>
+          <Text>flexDirection 定义元素的主轴方向</Text>  
+          <Text>justifyContent 定义伸缩项目在主轴线的对其方式</Text> 
+          <Text>alignItems 定义项目在侧轴上的对其方式</Text>   
+        </View>
+        
     );
   }
 }
-var styles2 = StyleSheet.create({
-  viewStyle : {
-    width:200,
-    height:100,
-    backgroundColor:"red"
-  },
-  textStyle : {
+
+var viewStyles = StyleSheet.create({
+  container:{
     flex:1,
-  }
-})
-
-
-const styles = StyleSheet.create({
-  container: {
+    backgroundColor: '#F5FCFF',
+    justifyContent : "center",
+  },
+  box1 : {
+    marginTop:20,
+    backgroundColor:"green",
     flexDirection:"row",
-    flexDirection:"column",
-    flex: 1,
+    justifyContent : "space-between",
+  },
+  box2 : {
+    marginTop :30,
+    backgroundColor:"yellow",
+    flexDirection:"row",
+    alignItems:"center"
+
+  },
+  description : {
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  flex_wrap : {
+    flexDirection:"row",
+    flexWrap:'wrap',
+
+  }, 
 });
+
+
