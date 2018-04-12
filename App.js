579d1bc99883217ab9d7e78480125f5d297c36d2
hengyang_King <hengyang.king@mymusichome.com>
2018-04-12 13:42:55 +0800

    add image

--- App.js
+++ App.js
@@ -12,9 +12,6 @@ import {
   StyleSheet,
   Text,
   View,
-
-
-
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -37,11 +34,27 @@ export default class App extends Component < Props > {
         <Text style = {{alignSelf:"flex-end"}}>当前屏幕的宽度:{Dimensions.get('window').width}</Text>
         <Text>当前屏幕的高度:{Dimensions.get('window').height}</Text>
         <Text>当前屏幕的分辨率:{Dimensions.get('window').sc}</Text>
-        <View>
-          {/* <Image source={require("/img/notify_button_left.png")} />*/}
 
+        <View>
+          {/*从项目中加载图片*/}
+          <Text>1.从项目中加载图片</Text>
+          {/* <Image source={require('./img/icon.png')} style={styles.imageStyle} />*/}
+
+          {/*从资源包中加载图片*/}
+          <Text>2.从APP中加载图片</Text>
+          {/*<Image source={require('image!bd_logo1')} style={styles.imageStyle}/>*/}
           
+
+          {/*从网络中加载图片*/}
+           <Text>3.从网络中加载图片</Text>
+          {/*<Image source={{uri: 'http://www.520it.com/userfiles/1/images/cms/site/2015/09/index_06.jpg'}}  style={styles.imageStyle} />*/}
+
+          {/*从资源包中加载图片--另一种方式*/}
+          <Text>4.从APP中加载图片</Text>
+          {/*<Image source={{uri: 'bd_logo1'}} style={styles.imageStyle} />*/}
+
         </View>
+
       </View> 
     );
   }
@@ -53,5 +66,16 @@ var viewStyles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     flex: 1,
+  },
+  imageStyle : {
+      width:160,
+      height:160,
+      // 圆角
+      // borderRadius:30
+
+      // 设置图片的内容模式
+      resizeMode:'cover'
+      // resizeMode:'contain'
+      // resizeMode:'stretch'
   }
 });
