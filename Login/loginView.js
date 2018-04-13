98d33e43aae424f74d25fc8475dfdf66be93aa90
hengyang_King <hengyang.king@mymusichome.com>
2018-04-13 19:00:08 +0800

    finish login view

--- Login/loginView.js
+++ Login/loginView.js
@@ -18,19 +18,45 @@ class loginView extends Component {
         return (
             <View style={styles.container}>
             	{/*头像*/}
-            	<Image/>
+            	<Image source = {require("../img/icon.png")} style = {styles.icon} />
  				{/*账号和密码*/}
- 				<TextInput/>
- 				<TextInput/>
+ 				<TextInput 
+ 					placeholder = {'请输入用户名'}
+ 				 	style = {styles.input}
+ 				 />
+ 				<TextInput
+					placeholder = {'请输入密码'}
+					password = {true}
+ 					style = {styles.input}
+ 				/>
+
+
+
 				{/*登录*/}
-				<View>
-					<View>
+				<View style = {{width:(width*9)/10.0}}>
+					<View style = {styles.loginBtn}>
 						<Text>登录</Text>
 					</View>
 
+					{/*设置*/}
+					<View style = {styles.loginSet}>
+						<Text>无法登录</Text>
+						<Text>注册用户</Text>
+					</View>
 				</View>
+				{/*其他登录方式*/}
+				<View style = {styles.exception}>
 
-				
+					<View style = {{justifyContent:'center',alignItems:'center'}}>
+						<Text>其他方式登录</Text>
+					</View>
+
+					<View style = {styles.exceptionBtns}>
+						<Image source = {require("../img/icon3.png")}/>
+						<Image source = {require("../img/icon7.png")}/>
+						<Image source = {require("../img/icon8.png")}/>
+					</View>
+				</View>
             </View>
         );
     }
@@ -40,7 +66,50 @@ var styles = StyleSheet.create({
 	container:{
 		flex:1,
 		backgroundColor:'#dddddd',
+		//设置侧轴对其方式
+		alignItems : "center",
+	},
+	icon : {
+		width:width/4.0,
+		height:width/4.0,
+		borderRadius: width/8.0,
+        marginTop: 80,
+        marginBottom:30,
+        borderWidth:2,
+        borderColor:'#fff'
+	},
+	input:{
+		width:width,
+		height:40,
+ 		backgroundColor:'#fff',
+        marginBottom:1,
+        textAlign:'center'
+	},
+	loginBtn : {
+		height:40,
+		backgroundColor:"#e9232c",
+		marginTop:30,
+        marginBottom:20,
+        borderRadius:5,
+         /*主侧轴居中*/
+        justifyContent:'center',
+        alignItems:'center',
 
+	},
+	loginSet : {
+		flexDirection:"row",
+		justifyContent:"space-between",
+	},
+	exception : {
+		position:'absolute',
+        left:10,
+        bottom:10, 
+        right:10, 
+	},
+	exceptionBtns : {
+		flexDirection:"row",
+		marginTop:20,
+		justifyContent:'space-around'
 	}
 })
 // 输出类
