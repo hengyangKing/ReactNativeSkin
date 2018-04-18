713c936e9aa081ac724001d528200ff102e62e65
hengyang_King <hengyang.king@mymusichome.com>
2018-04-18 14:13:44 +0800

    add scrollview

--- App.js
+++ App.js
@@ -9,21 +9,38 @@ import {
     Platform,
     StyleSheet,
     View,
-    
+	ScrollView,  
+	Text,  
 
 } from 'react-native';
 
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View>
-
-        
-      </View>
-    );
-  }
+	render() {
+  		return (
+      		<View>
+				<ScrollView horizontal = {true}>
+					{this.renderChildView()}
+				</ScrollView>		
+      		</View>
+    	);
+  	}
+  	renderChildView(){
+  		var allChild = [];
+  		var colors = ["red","green","yellow","purple","blue"];
+  		for (var i = 0; i < 20; i++) {
+  			allChild.push(
+				<View 
+					style = {{backgroundColor:colors[i%colors.length],width:100,height:200}}
+					key = {i}
+				>
+					<Text>{i}</Text>
+				</View>
+  			)
+  		}
+  		return allChild;
+  	}
 }
 
 
