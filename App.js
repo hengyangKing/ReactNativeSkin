d6d6c5b835a436f6284495e3586ff6098875f2ba
hengyang_King <hengyang.king@mymusichome.com>
2018-04-18 19:37:01 +0800

    正常显示

--- App.js
+++ App.js
@@ -27,7 +27,6 @@ export default class App extends Component<Props> {
   		return (
       		<View>
       			{this.renderAddBanner()}
-      			{this.renderLog()}		
       		</View>
     	);
   	}
@@ -36,10 +35,7 @@ export default class App extends Component<Props> {
   		
   		return [
   				<ScrollView horizontal = {true} key = {101}>
-  					{/*{this.renderChildView()}*/}
   					{this.renderImages()}
-					
-  				
 				</ScrollView>
 				];
 	}
@@ -48,43 +44,13 @@ export default class App extends Component<Props> {
 		var images = [];
 		for (var i = 0; i < imagesArr.length; i++) {
 			//imagesArr[i].img
-			let path = <Image key={i} source={require('./img/'+'img_01'+'.png')} style={{width:100,height:200}} />
+			let path = <Image key={i} source={{uri:imagesArr[i].img}} style={{width:width,height:200}} />
 			images.push(path);
-			console.log(path)
 		}
 		return images;
 	}
-	renderLog(){
-		var dataArr = data;
-		var logs = [];
-		for (var i = 0; i < dataArr.length; i++) {
 
-			let path = "<Image key={i} source={require('./img/"+dataArr[i].img+".png')} style={{width:100,height:200}} />"
-			logs.push(
-				<View style = {{marginTop:20}} key = {i}>
-					<Text>{path}</Text>
-				</View>
-
-			);
-		}
-		return logs;
-	}
-
-  	renderChildView(){
-  		var allChild = [];
-  		var colors = ["red","green","yellow","purple","blue"];
-  		for (var i = 0; i < 20; i++) {
-  			allChild.push(
-				<View 
-					style = {{backgroundColor:colors[i%colors.length],width:100,height:200}}
-					key = {i}
-				>
-					<Text>{i}</Text>
-				</View>
-  			)
-  		}
-  		return allChild;
-  	}
+  	
   	//注册计时器
   	mixins:[TimerMixin]
 
