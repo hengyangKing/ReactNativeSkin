917b62e1e9d5c74b01b89b155d02e8c20e537b81
hengyangKing <hengyangking@gmail.com>
2018-04-21 14:56:40 +0800

    添加tabbar & item

--- Component/Main.js
+++ Component/Main.js
@@ -3,19 +3,79 @@ import {
     Platform,
     StyleSheet,
     View,
+    TabBarIOS,
+    NavigatorIOS,
 } from 'react-native';
 //引入外部组件
 
 
 type Props = {};
+var {width ,height,scale} = require("Dimensions").get("window");
 export default class Main extends Component<Props> {
-  render() {
-    return (
-		<View style = {{flex:1,backgroundColor:"red"}}>
-			
-		</View>
-    );
-  }
+	constructor(Props){  
+        super(Props);    
+        this.state = {  
+           	selectedItem :"home",  
+	    };  
+    } 
+	render() {
+    	return (
+    		<TabBarIOS
+    			//TabBarIOS.Item selectedImage 属性无效 利用tintColor 实现选中高亮
+				tintColor = "orange"
+    		>
+    	 		{/*home*/}
+    	 		<TabBarIOS.Item	
+					icon={{uri: "tabbar_home",scale:scale}}
+					title = "首页"
+					onPress = {()=>this.onPress("home")}
+					selected = {this.state.selectedItem == "home"}
+    	 		>
+					<View>
+						
+					</View>
+	    	 	</TabBarIOS.Item>
+	    	 	{/*massage*/}
+	    	 	<TabBarIOS.Item
+					icon={{uri: "tabbar_message_center",scale:scale}}
+					title = "消息"
+					onPress = {()=>this.onPress("message")}
+					selected = {this.state.selectedItem == "message"}
+	    	 	>
+					<View>
+						
+					</View>
+	    	 	</TabBarIOS.Item>
+	    	 	{/*discover*/}
+	    	 	<TabBarIOS.Item
+					icon={{uri: "tabbar_discover",scale:scale}}
+					title = "发现"
+					onPress = {()=>this.onPress("discover")}
+					selected = {this.state.selectedItem == "discover"}
+	    	 	>
+	    	 		<View>
+						
+					</View>
+
+	    	 	</TabBarIOS.Item>
+	    	 	{/*main*/}
+	    	 	<TabBarIOS.Item
+					icon={{uri: "tabbar_profile",scale:scale}}
+					title = "我的"
+					onPress = {()=>this.onPress("profile")}
+					selected = {this.state.selectedItem == "profile"}
+	    	 	>
+					<View>
+						
+					</View>
+	    	 	</TabBarIOS.Item>
+	    	 </TabBarIOS>
+    	);
+  	}
+  	onPress(event){
+		console.log(event);
+		this.setState({selectedItem:event});	
+	} 
 }
 
 // 输出类
